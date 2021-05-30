a3e3d84daf3ae7b9be7598a2208b4e6dcf92cb51
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2021-05-30 12:54:15 +0200

    Update Post header

--- src/lib/components/PostHeader.svelte
+++ src/lib/components/PostHeader.svelte
@@ -1,7 +1,7 @@
-<script>
+<script lang="ts">
   import Tag from "$lib/components/Tag.svelte";
-  export let title;
-  export let published_at;
+  export let title: string;
+  export let published_at: string;
   export let tags;
 </script>
 
@@ -13,10 +13,14 @@
   <h1 class="font-sans pt-6 pb-2 text-3xl md:text-4xl" >
     {title}
   </h1>
+  {#if published_at != undefined && published_at.length > 0}
   <p class="text-sm md:text-base font-normal text-gray-500">
     Published { published_at }
   </p>
-  <p class="text-sm font-normal text-gray-500">
-    Tags: {#each tags as tag} <Tag {tag} /> {/each}
-  </p>
+  {/if}
+  {#if tags != undefined && tags.length > 0}
+    <p class="text-sm font-normal text-gray-500">
+      Tags: {#each tags as tag} <Tag {tag} /> {/each}
+    </p>
+  {/if}
 </div>
